b74a066613fa11eb8742c120315b28256010b819
Tobias Hess <mail.ts.hess@gmail.com>
2020-04-12 18:52:28 +0200

    created spring boot project. Closes #1.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # spring-chuck-norris-webapp
 Example Application that displays random chuck norris jokes on a simple web page.
+
+This was created in the context of the spring framework guru's Udemy course *Spring Framework 5: Beginner to Guru*. 
---
+++ src/main/java/de/framer/springchucknorriswebapp/SpringChuckNorrisWebappApplication.java
@@ -0,0 +1,13 @@
+package de.framer.springchucknorriswebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringChuckNorrisWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringChuckNorrisWebappApplication.class, args);
+	}
+
+}
