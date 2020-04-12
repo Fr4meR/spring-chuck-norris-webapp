9b362b23eb30b9695af38a2df37b029b42ab595d
Tobias Hess <mail.ts.hess@gmail.com>
2020-04-12 19:21:46 +0200

    created controller for the chuck norris quotes app. Closes #4 #5 #6 #7.

---
+++ src/main/java/de/framer/springchucknorriswebapp/controllers/ChuckNorrisQuotesController.java
@@ -0,0 +1,32 @@
+package de.framer.springchucknorriswebapp.controllers;
+
+import de.framer.springchucknorriswebapp.services.ChuckNorrisQuotesService;
+import org.springframework.boot.actuate.endpoint.web.annotation.RestControllerEndpoint;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.thymeleaf.model.IModel;
+
+/**
+ * {@link Controller} for the chuck norris quotes app
+ */
+@Controller
+public class ChuckNorrisQuotesController {
+
+    private final ChuckNorrisQuotesService quotesService;
+
+    /**
+     * Constructor
+     * @param quotesService
+     *      The {@link ChuckNorrisQuotesService} to use for getting quotes.
+     */
+    public ChuckNorrisQuotesController(ChuckNorrisQuotesService quotesService) {
+        this.quotesService = quotesService;
+    }
+
+    @RequestMapping("/")
+    public String getRandomQuote(Model model) {
+        model.addAttribute("joke", quotesService.getRandomQuote());
+        return "chucknorris";
+    }
+}
