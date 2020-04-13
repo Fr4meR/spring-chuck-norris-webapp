4458b61f9cffdd08f342bb4a5de989e8555d1644
Tobias Hess <mail.ts.hess@gmail.com>
2020-04-13 11:33:37 +0200

    refactored for an xml config to bring in chuck norris quotes app

--- src/main/java/de/framer/springchucknorriswebapp/SpringChuckNorrisWebappApplication.java
+++ src/main/java/de/framer/springchucknorriswebapp/SpringChuckNorrisWebappApplication.java
@@ -3,8 +3,10 @@ package de.framer.springchucknorriswebapp;
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-quotes-config.xml")
 public class SpringChuckNorrisWebappApplication {
 
 	public static void main(String[] args) {
--- src/main/java/de/framer/springchucknorriswebapp/config/ChuckNorrisQuotesConfig.java
+++ src/main/java/de/framer/springchucknorriswebapp/config/ChuckNorrisQuotesConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ChuckNorrisQuotesConfig {
 
-    @Bean
+//    @Bean
     public ChuckNorrisQuotes chuckNorrisQuotes(){
         return new ChuckNorrisQuotes();
     }
